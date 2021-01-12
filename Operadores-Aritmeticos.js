114836d29b58471bd8c5dbcc21099c1ad458a533
Amyr Kamel e Silva <67295939+amyrks1992@users.noreply.github.com>
2021-01-12 19:18:22 -0300

    Add files via upload

---
+++ Operadores-Aritmeticos.js
@@ -0,0 +1,45 @@
+var num1 = 8
+var num2 = 2
+
+//Soma//
+
+ soma = num1 + num2 
+
+//Subtração//
+
+sub = num1 - num2 
+
+//Multiplicação//
+
+mult = num1 * num2
+
+//Divisão//
+
+div = num1 / num2
+
+//Resto da Divisão//
+
+restDiv = num1 % num2
+
+//Potenciação//
+
+expo = num1 ** 2
+
+console.log (soma)
+console.log (sub)
+console.log (mult)
+console.log (div)
+console.log (restDiv)
+
+//Simplificação de Expressões//
+
+var num3 = 5
+
+//sem Simplificação num3 = num3 + 5//
+
+//com Simplificação// 
+
+num3 += 5
+num3 -= 2
+
+console.log (num3)
